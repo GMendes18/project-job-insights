3668b903ec4c036470ca7b49412bd101185847cd
Gabriel <gabri.mevial@gmail.com>
2024-03-01 16:40:49 -0300

    requisito 1 completo

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,6 +1,10 @@
-# from src.pre_built.counter import count_ocurrences
-
+from src.pre_built.counter import count_ocurrences
 
 def test_counter():
-    raise
 
+    path = "data/jobs.csv"
+    javascript_counter = count_ocurrences(path, "javascript")
+    assert javascript_counter == 122
+
+    python_counter = count_ocurrences(path, "python")
+    assert python_counter == 1639
